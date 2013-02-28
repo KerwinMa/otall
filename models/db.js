29faaa8645a59c7b6c0ef2b7fe94d3c9fb640ecc
Wu Hao <hao.wu@dreamingame.com>
2013-02-28 13:42:46 +0800

    add db wrapper to avoid open multitimes"

--- models/db.js
+++ models/db.js
@@ -3,8 +3,38 @@ var Db = require('mongodb').Db;
 var Connection =  require('mongodb').Connection;
 var Server = require('mongodb').Server;
 
-module.exports = new Db(settings.db, new Server(settings.host, settings.dbport, {
+var mydb = new Db(settings.db, new Server(settings.host, settings.dbport, {
 	auto_reconnect : true,
 	journal: true,
 	fsync: true
 }));
+
+function DbWrapper()
+{
+};
+
+DbWrapper.refCount = 0;
+
+DbWrapper.open = function open(callback){
+	if(DbWrapper.refCount==0){
+		mydb.open(function(){
+			callback(null, mydb);
+			//console.log('open db: ref='+DbWrapper.refCount);
+		});
+	}
+	else{
+		callback(null, mydb);
+	}
+	
+	DbWrapper.refCount++;
+};
+
+DbWrapper.close = function close(){
+	DbWrapper.refCount--;
+	if(DbWrapper.refCount==0){
+		mydb.close();	
+		//console.log('close db');
+	}
+};
+
+module.exports = DbWrapper;
